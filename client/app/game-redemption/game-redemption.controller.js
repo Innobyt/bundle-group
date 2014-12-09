910898937b7341e8ee2acc77100c5a38994294c8
Kyle Haithir <kyle@innobyt.com>
2014-12-09 14:41:41 +0800

    fix [Error] SyntaxError: Unexpected identifier 'email'. Expected '}' to end a object literal. (anonymous function) (game-redemption.controller.js, line 26)
    fix Error: [ng:areq] Argument 'GameRedemptionCtrl' is not a function, got undefined

--- client/app/game-redemption/game-redemption.controller.js
+++ client/app/game-redemption/game-redemption.controller.js
@@ -16,33 +16,33 @@
 		};	
 		
 		// Ng-Table to display User's Claimed Redemption Keys, the data consists of username, email, bundlename, gamename, redemptionkey, timestamp (see below under filter for each of the description)
-		/*var data = $scope.readall();	
+		var data = $scope.readall();	
 			
 			 $scope.tableParams = new ngTableParams({
-	         page: 1,            // show first page
-	         count: 10,          // count per page
-	         filter: {
-	             username: 'JohnDoe'       // initial filter for username
-				 email: '' //  User's email
-				 bundlename: '' //  Display the specified bundlename
-				 gamename: '' // Array of the game title names tagged with bundlename
-				 redemptionkey: '' // Claimed Redemption Key that User has claimed
-				 timestamp: '' // Claimed Date & Time
-	         }
-	     }, {
-	         total: data.length, // length of data
-	         getData: function($defer, params) {
-	             // use build-in angular filter
-	             var orderedData = params.filter() ?
-	                    $filter('filter')(data, params.filter()) :
-	                    data;
+		         page: 1,            			// show first page
+		         count: 10,          			// count per page
+		         filter: {
+		             username: 'JohnDoe',    	// initial filter for username
+					 email: '', 				//  User's email
+					 bundlename: '', 			//  Display the specified bundlename
+					 gamename: '', 				// Array of the game title names tagged with bundlename
+					 redemptionkey: '', 		// Claimed Redemption Key that User has claimed
+					 timestamp: '', 			// Claimed Date & Time
+		         }
+		     }, {
+		         total: data.length, // length of data
+		         getData: function($defer, params) {
+		             // use build-in angular filter
+		             var orderedData = params.filter() ?
+		                    $filter('filter')(data, params.filter()) :
+		                    data;
 
-	             $scope.gameredemption = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
+		             $scope.gameredemption = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
 
-	             params.total(orderedData.length); // set total for recalc pagination
-	             $defer.resolve($scope.gameredemption);
-	         }
-	     });*/
+		             params.total(orderedData.length); // set total for recalc pagination
+		             $defer.resolve($scope.gameredemption);
+		         }
+		     });
 		 
 			// post, gameRedemption creation ('C' in Crud)
 			$scope.submit = function() {
