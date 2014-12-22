018abf4fa1c2ab0eba93ee0914584afd336d7c57
Kyle Haithir <kyle@innobyt.com>
2014-12-22 11:13:32 +0800

    update code format, pretty

--- server/api/game-bundle/gamebundle.model.js
+++ server/api/game-bundle/gamebundle.model.js
@@ -4,13 +4,13 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var GamebundleSchema = new Schema({
-    gamelist    : Array,			// ['tome radider', 'resident evil', 'descriction derby']
-    redemptions : Array,			// [{ key, status}, { key, status }]
-    merchant    : String,
-    merchant_prefix    : String,	// new merchant prefix
-    bundlename  : String,
-    threshold	: Number,
-    count		: Number
+    gamelist    		: Array,	// example: ['tomb radider', 'resident evil', 'destruction derby']
+    redemptions 		: Array,	// example: [{ key, status}, { key, status }]
+    merchant    		: String,
+    merchant_prefix    	: String,
+    bundlename  		: String,
+    threshold			: Number,
+    count				: Number
 });
 
 module.exports = mongoose.model('gamebundle', GamebundleSchema);
