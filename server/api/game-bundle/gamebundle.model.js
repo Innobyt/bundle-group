e30373bd33dc6fea91c4ad9cc77dd4b369879d84
Kyle Haithir <kyle@innobyt.com>
2015-01-05 09:52:32 +0800

    fix wrong variable field name

--- server/api/game-bundle/gamebundle.model.js
+++ server/api/game-bundle/gamebundle.model.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose'),
 
 var GamebundleSchema = new Schema({
 	gamelist:			Array, // example: [{ 'gametitle' : 'tomb radider'}, {'gametitle' : 'resident evil'}]
-	redemptionskey:		Array, // example: [{ key, status}, { key, status }]
+	redemptions:		Array, // example: [{ key, status}, { key, status }]
 	merchant:			String,
 	merchant_prefix:	String,
 	bundlename:			String,
